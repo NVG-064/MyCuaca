4e402e7d4cee4744c71d7f25f718a6218a162530
NVG-064 <aryslow92@gmail.com>
2025-01-06 15:26:41 +0700

    view: Initial WeatherCurrent view

--- app/src/main/java/io/github/nvg064/mycuaca/ui/view/activity/WeatherCurrentActivity.kt
+++ app/src/main/java/io/github/nvg064/mycuaca/ui/view/activity/WeatherCurrentActivity.kt
@@ -1,16 +1,17 @@
-package io.github.nvg064.mycuaca
+package io.github.nvg064.mycuaca.ui.view.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.github.nvg064.mycuaca.R
 
-class MainActivity : AppCompatActivity() {
+class WeatherCurrentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.weather_current_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
